4d740dbf683f9857427f880ef81fa818bf82ebfb
Xuyining <oxuyining@gmail.com>
2018-04-22 18:03:52 +0800

    SIFT关键点
    Signed-off-by: Xuyining <oxuyining@gmail.com>

--- basis/sift template match/haha.py
+++ basis/sift template match/haha.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 MIN_MATCH_COUNT = 4
 
-imgname1 = "mouse.jpg"
+imgname1 = "test1.jpg"
 
 ## (1) prepare data
 img1 = cv2.imread(imgname1)
@@ -15,9 +15,21 @@ sift = cv2.xfeatures2d.SIFT_create()
 ## (4) Detect keypoints and compute keypointer descriptors
 kpts1, descs1 = sift.detectAndCompute(gray1,None)
 
+times = 0
+for i in range(len(kpts1)):
+    if i % 3 == 0:
+        del kpts1[i - times]
+        times = times + 1
+times = 0
+for i in range(len(kpts1)):
+    if i % 3 == 0:
+        del kpts1[i - times]
+        times = times + 1
+
 ## (8) drawMatches
 # matched = cv2.drawKeypoints(img1,kpts1)
 # matched = cv2.drawKeypoints(img1,kpts1,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+matched = cv2.drawKeypoints(img1, kpts1, img1, (0, 0 ,0))
 
 ## (10) save and display
 cv2.imwrite("matched.png", matched)
--- basis/sift template match/test.py
+++ basis/sift template match/test.py
@@ -25,7 +25,7 @@ kpts2, descs2 = sift.detectAndCompute(gray2,None)
 ## (5) knnMatch to get Top2
 matches = matcher.knnMatch(descs1, descs2, 2)
 # Sort by their distance.
-matches = sorted(matches, key = lamb`da x:x[0].distance)
+matches = sorted(matches, key = lambda x:x[0].distance)
 
 ## (6) Ratio test, to get good matches.
 good = [m1 for (m1, m2) in matches if m1.distance < 0.8 * m2.distance]
@@ -35,39 +35,39 @@ canvas2 = img2.copy()
 
 ## (7) find homography matrix
 ## 当有足够的健壮匹配点对（至少4个）时
-if len(good)>=MIN_MATCH_COUNT:
-    ## 从匹配中提取出对应点对
-    ## (queryIndex for the small object, trainIndex for the scene )
-    src_pts = np.float32([ kpts1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kpts2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-    ## find homography matrix in cv2.RANSAC using good match points
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    ## 掩模，用作绘制计算单应性矩阵时用到的点对
-    #matchesMask2 = mask.ravel().tolist()
-    ## 计算图1的畸变，也就是在图2中的对应的位置。
-    h,w = img1.shape[:2]
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
-    ## 绘制边框
-    cv2.polylines(canvas,[np.int32(dst)],True,(0,255,0),3, cv2.LINE_AA)
-    cv2.polylines(canvas2, [np.int32(dst)], True, (0, 255, 0), 3, cv2.LINE_AA)
-else:
-    print( "Not enough matches are found - {}/{}".format(len(good),MIN_MATCH_COUNT))
+# if len(good)>=MIN_MATCH_COUNT:
+#     ## 从匹配中提取出对应点对
+#     ## (queryIndex for the small object, trainIndex for the scene )
+#     src_pts = np.float32([ kpts1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+#     dst_pts = np.float32([ kpts2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+#     ## find homography matrix in cv2.RANSAC using good match points
+#     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+#     ## 掩模，用作绘制计算单应性矩阵时用到的点对
+#     #matchesMask2 = mask.ravel().tolist()
+#     ## 计算图1的畸变，也就是在图2中的对应的位置。
+#     h,w = img1.shape[:2]
+#     pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+#     dst = cv2.perspectiveTransform(pts,M)
+#     ## 绘制边框
+#     cv2.polylines(canvas,[np.int32(dst)],True,(0,255,0),3, cv2.LINE_AA)
+#     cv2.polylines(canvas2, [np.int32(dst)], True, (0, 255, 0), 3, cv2.LINE_AA)
+# else:
+#     print( "Not enough matches are found - {}/{}".format(len(good),MIN_MATCH_COUNT))
 
 
 ## (8) drawMatches
 matched = cv2.drawMatches(img1,kpts1,canvas,kpts2,good,None)#,**draw_params)
 
 ## (9) Crop the matched region from scene
-h,w = img1.shape[:2]
-pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts,M)
-perspectiveM = cv2.getPerspectiveTransform(np.float32(dst),pts)
-found = cv2.warpPerspective(img2,perspectiveM,(w,h))
+# h,w = img1.shape[:2]
+# pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+# dst = cv2.perspectiveTransform(pts,M)
+# perspectiveM = cv2.getPerspectiveTransform(np.float32(dst),pts)
+# found = cv2.warpPerspective(img2,perspectiveM,(w,h))
 
 ## (10) save and display
 cv2.imwrite("matched.png", matched)
 cv2.imshow("matched", matched)
-cv2.imwrite("result.png", canvas2)
-cv2.imshow("matche", canvas2)
+# cv2.imwrite("result.png", canvas2)
+# cv2.imshow("matche", canvas2)
 cv2.waitKey();cv2.destroyAllWindows()
